f34296b5524ba7395716b7e5983a10e69a39ce1a
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-07-08 20:38:23 +0530

    Use morgan for logging requests

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
+if (process.env.NODE_ENV !== "production")
+    app.use((0, morgan_1.default)("dev"));
+app.get("/", (_req, res) => {
+    res.send("Hello world!");
+});
 app.get("/api", (_req, res) => {
-    res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.end(`Hello! Shefa`);
+    res.send(`Hello! Shefa`);
 });
 app.get("/api/item/:slug", (req, res) => {
     const { slug } = req.params;
-    res.end(`Item: ${slug}`);
+    res.send(`Item: ${slug}`);
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);

daa6145722b65ed5248876ae3045f237d17a21c5
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-07-08 21:57:10 +0530

    Port API to tRPC in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,49 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.appRouter = void 0;
+const server_1 = require("@trpc/server");
+const trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
-const port = process.env.PORT || 3000;
+// created for each request
+const createContext = ({ req, res, }) => ({}); // no context
+const t = server_1.initTRPC.context().create();
+exports.appRouter = t.router({
+    getUser: t.procedure.query(() => {
+        return { name: "Bilbo" };
+    }),
+});
 const app = (0, express_1.default)();
 if (process.env.NODE_ENV !== "production")
     app.use((0, morgan_1.default)("dev"));
-app.get("/", (_req, res) => {
-    res.send("Hello world!");
-});
-app.get("/api", (_req, res) => {
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.send(`Hello! Shefa`);
-});
-app.get("/api/item/:slug", (req, res) => {
-    const { slug } = req.params;
-    res.send(`Item: ${slug}`);
-});
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
-});
+app.use("/trpc", trpcExpress.createExpressMiddleware({
+    router: exports.appRouter,
+    createContext,
+}));
+app.listen(4000);

bc7cfc622b24eb6bfdf855518c21ee3030725614
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-07-08 22:41:26 +0530

    Integrate with Prisma and PlanetScale

--- dist/index.js
+++ dist/index.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -31,15 +40,40 @@ const server_1 = require("@trpc/server");
 const trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const zod_1 = require("zod");
+const client_1 = require("@prisma/client");
+const primsa = new client_1.PrismaClient();
 // created for each request
 const createContext = ({ req, res, }) => ({}); // no context
 const t = server_1.initTRPC.context().create();
 exports.appRouter = t.router({
-    getUser: t.procedure.query(() => {
-        return { name: "Bilbo" };
-    }),
+    get: t.procedure.input(zod_1.z.number()).query((opts) => __awaiter(void 0, void 0, void 0, function* () {
+        opts.input; // number
+        const user = yield primsa.user.findMany({
+            where: {
+                id: opts.input,
+            },
+        });
+        return user;
+    })),
+    create: t.procedure
+        .input(zod_1.z.object({ email: zod_1.z.string(), name: zod_1.z.string() }))
+        .mutation((opts) => __awaiter(void 0, void 0, void 0, function* () {
+        const output = yield primsa.user.create({
+            data: {
+                email: opts.input.email,
+                name: opts.input.name,
+            },
+        });
+        return output;
+    })),
 });
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "*",
+    credentials: true,
+}));
 if (process.env.NODE_ENV !== "production")
     app.use((0, morgan_1.default)("dev"));
 app.use("/trpc", trpcExpress.createExpressMiddleware({
--- trpc/index.ts
+++ trpc/index.ts
@@ -2,7 +2,11 @@ import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const primsa = new PrismaClient();
 
 // created for each request
 const createContext = ({
@@ -14,14 +18,39 @@ type Context = inferAsyncReturnType<typeof createContext>;
 const t = initTRPC.context<Context>().create();
 
 export const appRouter = t.router({
-  getUser: t.procedure.query(() => {
-    return { name: "Bilbo" };
+  get: t.procedure.input(z.number()).query(async (opts) => {
+    opts.input; // number
+    const user = await primsa.user.findMany({
+      where: {
+        id: opts.input,
+      },
+    });
+    return user;
   }),
+  create: t.procedure
+    .input(z.object({ email: z.string(), name: z.string() }))
+    .mutation(async (opts) => {
+      const output = await primsa.user.create({
+        data: {
+          email: opts.input.email,
+          name: opts.input.name,
+        },
+      });
+      return output;
+    }),
 });
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
+
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
